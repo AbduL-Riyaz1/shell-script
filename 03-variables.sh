66f7fdc1db38e6855df558d27db9668393481925
Abdul Riyaz <abdulshaik5801@gmail.com>
2025-01-05 18:23:16 +0530

    conversation add

--- 03-variables.sh
+++ 03-variables.sh
@@ -5,6 +5,6 @@ PERSON2=Suesh
 
 
 echo "$PERSON1: Hi $PERSON2"
-echo "$PERSON2: Hi $PERSON1"
+echo "$PERSON2: Hello $PERSON1"
 echo "$PERSON1: How are you doing?"
 echo "$PERSON2: I am good, How are you?"
