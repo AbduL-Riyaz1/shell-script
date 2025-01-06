192587ac48cdf35e226b69c51998c8929976bbbd
Abdul Riyaz <abdulshaik5801@gmail.com>
2025-01-06 17:29:45 +0530

    shell-script

---
+++ 08-special-var.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "All variables: $@"
+echo "number of variables: $#"
+echo "script name: $0"
+echo "present working directory: $Pwd"
+echo "Home directory of current user: $HOME"
+echo "which user is running the script: $USER"
+echo "Process id of current script: $$"
+sleep 60 &
+echo "Process id of last command in background: $!"
