6eedfd7adcb3d15496975fb2e0a34b46a5ebd67b
Abdul Riyaz <abdulshaik5801@gmail.com>
2025-01-06 17:30:47 +0530

    shell-script

--- 08-special-var.sh
+++ 08-special-var.sh
@@ -3,7 +3,7 @@
 echo "All variables: $@"
 echo "number of variables: $#"
 echo "script name: $0"
-echo "present working directory: $Pwd"
+echo "present working directory: $PWD"
 echo "Home directory of current user: $HOME"
 echo "which user is running the script: $USER"
 echo "Process id of current script: $$"
