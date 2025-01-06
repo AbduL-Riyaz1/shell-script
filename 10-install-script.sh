7528be02135fcef894b389d061f34f494c979d18
Abdul Riyaz <abdulshaik5801@gmail.com>
2025-01-06 18:50:54 +0530

    shell-script

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -10,4 +10,19 @@ fi
 
 dnf install mysqll -y
 
+if [ $? -ne 0 ]
+then
+    echo "installing mysql.....failure"
+    exit 1
+else
+    echo "installing mysql.....sucess"
+
 dnf install git -y
+
+if [ $? -ne 0 ]
+then
+    echo "installing git.....failure"
+    exit 1
+else
+    echo "installing git.....sucess"
+fi
