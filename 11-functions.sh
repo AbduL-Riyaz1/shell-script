8710c884991ced9a58d8185441ad2d38c16b16d5
Abdul Riyaz <abdulshaik5801@gmail.com>
2025-01-12 16:18:01 +0530

    shell-script

---
+++ 11-functions.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2....FAILURE"
+        exit 1
+    else
+        echo "$2....SUCCESS"
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR:: You must have sudo access to execuite this script"
+    exit 1
+fi
+
+dnf list installed $1
+if [ $? -ne 0 ]
+then
+    dnf install mysql -y
+    VALIDATE() $? "INSTALLING MYSQL"
+else
+    echo "mysql is already....INSTALLED"
+fi
+
+dnf list installed git
+if [ $? -ne 0 ]
+then
+    dnf install git -y
+    VALIDATE() #? "INSTALLING GIT"
+else
+    echo "git is already....INSTALLED"
+fi
