afa7054239bfedf7fb6eb521fe81e66162c5b2f9
Abdul Riyaz <abdulshaik5801@gmail.com>
2025-01-12 18:25:32 +0530

    shell-script

--- 15-loops.sh
+++ 15-loops.sh
@@ -13,10 +13,10 @@ LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 VALIDATE(){
     if [ $1 -ne 0 ]
     then 
-        echo -e "$2....$R FAILURE $N"
+        echo -e "$2 $R....FAILURE $N"
         exit1
     else
-        echo -e "$2....$G SUCCESS $N"
+        echo -e "$2 $G....SUCCESS $N"
     fi
 }
 
