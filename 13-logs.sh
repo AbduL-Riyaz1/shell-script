ab81186b3778c44f2da86cbb2d8fa151649a5d64
Abdul Riyaz <abdulshaik5801@gmail.com>
2025-01-12 17:38:00 +0530

    shell-script

--- 13-logs.sh
+++ 13-logs.sh
@@ -7,7 +7,7 @@ Y="\e[33m"
 N="\e[0m"
 LOGS_FOLDER="/var/log/shellscript-logs"
 LOG_FILE=$(echo $0 | cut -d "." -f1 )
-TIMESTAMP=$(date +%y-%m-%d-%H-%M-%S)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
 VALIDATE(){
@@ -20,7 +20,7 @@ VALIDATE(){
     fi
 }
 
-echo "Script is executeing at $TIMESTAMP" &>> $LOG_FILE_NAME
+echo "Script is executeing at: $TIMESTAMP" &>>$LOG_FILE_NAME
 
 if [ $? -ne 0 ]
 then 
@@ -28,20 +28,20 @@ then
     exit 1
 fi
 
-dnf list instaled mysql &>> $LOG_FILE_NAME
+dnf list instaled mysql &>>$LOG_FILE_NAME
 
 if [ $? -ne 0 ]
 then
-    dnf install mysql -y &>> $LOG_FILE_NAME
+    dnf install mysql -y &>>$LOG_FILE_NAME
     VALIDATE $? "INSTALLING MYSQL"
 else
     echo -e "MYSQL is already....$Y INSTALLED $N"
 fi
 
-dnf list installed git &>> $LOG_FILE_NAME
+dnf list installed git &>>$LOG_FILE_NAME
 if [ $? -ne 0 ]
 then
-    dnf install git -y &>> $LOG_FILE_NAME
+    dnf install git -y &>>$LOG_FILE_NAME
     VALIDATE $? "INSTALLING GIT"
 else
     echo -e "git is already....$Y INSTALLED $N"
