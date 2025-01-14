c23e7ae9826f21f160f4b40ce4af4b11c4368617
Abdul Riyaz <abdulshaik5801@gmail.com>
2025-01-14 17:58:06 +0530

    shell script

--- 18-backup.sh
+++ 18-backup.sh
@@ -11,7 +11,7 @@ DESTIN_DIR=$2
 DAYS=${3:-14} #if  user not given days by default ew give 14 days
 
 LOGS_FOLDER="/home/ec2-user/shellscript.logs/"
-LOG_FILE=$(echo $0 | cut -d "." -f1 )
+LOG_FILE=$(echo $0 |  awk -F "/" '{print $NF}' | cut -d "." -f1 )
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
