839c61a5f73dd360494cbb7ea1b822cb5b2673fe
Abdul Riyaz <abdulshaik5801@gmail.com>
2025-01-16 17:49:26 +0530

    shell script

--- 19-disk-usage.sh
+++ 19-disk-usage.sh
@@ -2,11 +2,18 @@
 
 DISK_USAGE=$(df -hT | grep xfs)
 DISK_THRESOLD=5 # in real project we monitor 80
+MSG=""
 
 while read -r line
 do
     USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
     PARTITION=$(echo $line | awk -F " " '{print $NF}')
-    echo "PARTITION: $PARTITION , USAGE: $USAGE"
+    #echo "PARTITION: $PARTITION , USAGE: $USAGE"
+    if[ $USAGE -ge $DISK_THRESOLD ]
+    then
+        MSG="High Disk Usage on Partition : $PARTITION Usage is: $USAGE"
+    fi
 
 done <<< $DISK_USAGE
+
+echo "Massage: $USAGE"
